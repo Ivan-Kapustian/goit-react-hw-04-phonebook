c90cd7e7486a72ea7449f99232abb63b6cd441e0
Ivan Kapustian <michiro2845@gmail.com>
2023-09-16 23:35:26 +0300

    done hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,17 @@ class App extends Component {
     number: '',
     filter: '',
   };
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   handleChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({
